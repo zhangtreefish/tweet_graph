2c5e12fcb48c95248c90a5a844d964a2b36d992c
zhangtreefish <zhangtreefish@users.noreply.github.com>
2016-04-06 21:43:47 -0500

    fix: move result line to the right block to calculate just once

--- src/average_degree.py
+++ src/average_degree.py
@@ -11,6 +11,7 @@ from datetime import datetime
 from collections import OrderedDict
 import sys
 
+
 def average_degree_graph(graph):
     """get average degree of a graph object"""
     edges = float(len(graph.edges()))
@@ -21,39 +22,56 @@ def average_degree_graph(graph):
     # print 'res:', edges, no_vertices, average_degree
     return '%.2f' % average_degree
 
+
 def main():
     try:
-        with open(sys.argv[1], 'r') as tweets, open (sys.argv[2], 'w') as output:
+        with open(sys.argv[1], 'r') as tweets, open(sys.argv[2], 'w') as output:
             # make a pretty global dictionary to store time and hashtag info
             hashtag_dict = OrderedDict()
             result = '0.00'
             # iterate over the input file, one line at a time
             for line in tweets:
                 tweet = line.decode('utf-8')
-                tweet_json = json.loads(tweet) # dict object
-                if 'created_at' in tweet_json: # needed to address keyError
+                tweet_json = json.loads(tweet)  # dict object
+                if 'created_at' in tweet_json:  # needed to address keyError
                     # get the time of the tweet
-                    created_at = datetime.strptime(tweet_json['created_at'].encode('utf-8'), "%a %b %d %H:%M:%S +0000 %Y")
+                    created_at = datetime.strptime(
+                        tweet_json['created_at'].encode('utf-8'),
+                        "%a %b %d %H:%M:%S +0000 %Y")
                     # get the time of the last tweet
                     if len(hashtag_dict.keys()) == 0:
                         time_last_tweet = created_at
                     else:
                         time_last_tweet = hashtag_dict.keys()[-1]
-                    # ensure to only address tweets not over 60 seconds older than the last
+                    # ensure to only address tweets not over 60 seconds older
+                    # than the last
                     time_delta = created_at - time_last_tweet
                     if time_delta.total_seconds() > - 60:
-                        if 'entities' in tweet_json:
-                            if 'hashtags' in tweet_json['entities']:
-                                hashtags = tweet_json['entities']['hashtags']
-                                hashtag_list = [i['text'].encode('utf-8').strip() for i in hashtags]
-                                # update hashtag_dict with tweet of at least 2 tags
-                                hashtag_list = list(set(hashtag_list)) # keep only unique tags
-                                if len(hashtag_list) > 1:
-                                    hashtag_dict[created_at] = hashtag_list
+                        # if 'entities' in tweet_json:
+                        if 'hashtags' in tweet_json['entities']:
+                            hashtags = tweet_json['entities']['hashtags']
+                            hashtag_list = [i['text'].encode('utf-8').strip()
+                                            for i in hashtags]
+
+                            # keep only unique tags
+                            hashtag_list = list(set(hashtag_list))
+                            # update hashtag_dict with tweet of at least 2 tags
+                            if len(hashtag_list) > 1:
+                                hashtag_dict[created_at] = hashtag_list
+                            # for those with 0 or 1 tag, the result is the
+                            # same as in the last
+                            else:
+                                output.write(str(result) + '\n')
+                                continue
+                        else:
+                            output.write(str(result) + '\n')
+                            continue
+                    # ignore tweets over 60 seconds older than the last
                     else:
                         continue
 
-                    # remove the tweets more than 60 second older than the current tweet
+                    # remove the tweets more than 60 second older than the
+                    # current tweet
                     sorted_key_list = sorted(hashtag_dict.keys())
                     for k in sorted_key_list:
                         time_elapsed = created_at - k
@@ -64,14 +82,16 @@ def main():
 
                     # finally, initialize the graph, and derive average degree
                     graph = Graph()
-                    for k in hashtag_dict:
-                        for tag in hashtag_dict[k]:
+                    for t in hashtag_dict:
+                        for tag in hashtag_dict[t]:
                             graph.add_vertex(tag)
-                        length = len(hashtag_dict[k])
+                        length = len(hashtag_dict[t])
                         for start in range(length - 1):
                             for end in range(start+1, length):
-                                graph.add_edge((hashtag_dict[k][start], hashtag_dict[k][end]))
-                        result = average_degree_graph(graph)
+                                graph.add_edge(
+                                    hashtag_dict[t][start],
+                                    hashtag_dict[t][end])
+                    result = average_degree_graph(graph)
 
                 # ignore those lines without 'created_at'
                 else:
@@ -79,7 +99,8 @@ def main():
                 # for each valid line, report the result
                 print result
                 output.write(str(result) + '\n')
-
+            else:
+                print 'All done!'
     except IOError as err:
         print 'File error:', str(err)
 
